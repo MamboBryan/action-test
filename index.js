5aa64a0c0e87511306921550562b796826a1e550
MamboBryan <mambobryan@gmail.com>
2022-06-28 15:43:07 +0300

    updated string concatnation in js

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const github = require("@actions/github");
 
 try {
   const whoToGreet = core.getInput("who-to-greet");
-  console.log("Hello ${whoToGreet}!");
+  console.log(`Hello ${whoToGreet}!`);
 
   const time = new Date().toTimeString();
   core.setOutput("time", time);
 
   const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log("This event payload ${payload}");
+  console.log(`This event payload ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
