1a5e0ba1359ee6d6350a902fa73670dfffce6980
MamboBryan <mambobryan@gmail.com>
2022-07-06 16:42:54 +0300

    updates dist/index.js

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ try {
   const payload = JSON.stringify(github.context.payload, undefined, 2);
 
   // log inputs
-  // console.log(`Hello ${name}!`);
-  console.log(`Hello World!`);
+  console.log(`Hello ${name}!`);
   console.log(`This event payload ${payload}`);
 
   // get outputs
