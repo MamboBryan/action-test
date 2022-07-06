5d10feadd1647a6709fe43a29f29c99400d5e97e
MamboBryan <mambobryan@gmail.com>
2022-07-06 15:56:46 +0300

    updates everything to see if it works

--- index.js
+++ index.js
@@ -2,14 +2,19 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 
 try {
-  const whoToGreet = core.getInput("who-to-greet");
-  console.log(`Hello ${whoToGreet}!`);
-
-  const time = new Date().toTimeString();
-  core.setOutput("time", time);
-
+  // get inputs
+  const name = core.getInput("who-to-greet");
   const payload = JSON.stringify(github.context.payload, undefined, 2);
+
+  // log inputs
+  console.log(`Hello ${name}!`);
   console.log(`This event payload ${payload}`);
+
+  // get outputs
+  const time = new Date().toTimeString();
+
+  // return outputs
+  core.setOutput("time", time);
 } catch (error) {
   core.setFailed(error.message);
 }
